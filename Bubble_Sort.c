7ba88b9ac2434b033289d9a577b0602d71a17b73
Bing <just.love.angel@gmail.com>
2016-11-06 16:58:46 +0800

    Normal Bubble Sort

---
+++ Bubble_Sort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void Swap(int *element1, int *element2) {
+	int tmp = 0;
+	tmp = *element1;
+	*element1 = *element2;
+	*element2 = tmp;
+}
+
+void Bubble_Sort (int arr[], int len) {
+	int i, j ;
+	for(i = 0; i < len -1; i++)
+		for(j = 0; j < len - i - 1; j++) {
+			if(arr[j] > arr[j + 1])
+				Swap(&arr[j], &arr[j + 1]);
+		}
+}
+
+int main(void) {
+	int data[] = {12,1,44,55,69,23,19,4,90,28};
+	int x = sizeof(data)/sizeof(data[0]);
+	
+	Bubble_Sort(data, x);
+
+	for(x = 0; x <= 10; x++)
+		printf("%d \n", data[x]);
+}
+
